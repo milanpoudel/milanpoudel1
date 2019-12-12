334ba863dbd2b9eb85e6098af062283e1b74d4c3
Milan Poudel <milanpowerpoudel@gmail.com>
2019-12-12 13:27:06 +0545

    Updates

---
+++ precache-manifest.1e345986f95bb654c2142116cbba59d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13508463f8acbf51db0c0609ba9e9035",
+    "url": "/milanpoudel1/index.html"
+  },
+  {
+    "revision": "878cce8dc4ec9a55d304",
+    "url": "/milanpoudel1/static/css/main.ac98d574.chunk.css"
+  },
+  {
+    "revision": "0153b6993f6aa2318413",
+    "url": "/milanpoudel1/static/js/2.e369c233.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/milanpoudel1/static/js/2.e369c233.chunk.js.LICENSE"
+  },
+  {
+    "revision": "878cce8dc4ec9a55d304",
+    "url": "/milanpoudel1/static/js/main.4f937171.chunk.js"
+  },
+  {
+    "revision": "7d5f67ea5e3e79173d15",
+    "url": "/milanpoudel1/static/js/runtime-main.fa9fa5d5.js"
+  }
+]);
